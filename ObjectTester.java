c13d3ec6181896367696c8d282d6bc4ee8e86b59
dylanmas <48572363+dylanmas@users.noreply.github.com>
2021-01-31 21:29:54 -0600

    Added object tester file

---
+++ ObjectTester.java
@@ -0,0 +1,11 @@
+public class ObjectTester {
+    public static void main(String[] args) {
+        Card test = new Card(13, 2);
+        Card foo = new Card(1, 2);
+        System.out.println(test.getSuitName());
+        System.out.println(test.compareTo(foo));
+
+        Deck lol = new Deck(1);
+        System.out.println(lol);
+    }
+}

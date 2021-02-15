9cefcfbaf4d11ad984d5dd83eda01912ea87a973
dylanmas <dylanmas687@gmail.com>
2021-02-14 21:54:37 -0600

    Added multiple methods to the Player class

--- ObjectTester.java
+++ ObjectTester.java
@@ -4,7 +4,7 @@ public class ObjectTester {
         Card foo = new Card(1, 2);
         System.out.println(test.getSuitName() + "jfsdjlkfsdjklfdsjkldsfakjl");
         System.out.println(test.compareTo(foo));
-
+        
         Deck lol = new Deck(1);
         System.out.println(lol);
     }
--- Player.java
+++ Player.java
@@ -4,22 +4,51 @@ public class Player {
     private String name = "";
     private ArrayList<Card> inventory;
     
-    public Player(){
+    public Player(String nameIn){
         inventory = new ArrayList<Card>();   
+        name = nameIn;
     }
 
-    //accessors
-    public String getPlayerCard(){
-        String output = "";
-        for (int i = 1; i < inventory.size(); i++) {
-            output += inventory.get(i);
-        }
-        return output;
+    //----------------------------------------------------------getter and setter methods:
+
+    public Card getPlayerCard(int index){
+        return inventory.get(index);
     }
 
-    //mutators
     public void addPlayerCard(Card adder){
         inventory.add(adder);
     }
 
+    public void removePlayerCard(int index){
+        inventory.remove(index);
+    }
+
+    public void removeTopPlayerCard(int){
+        inventory.remove(inventory.size() - 1);
+    }
+    
+
+    //the deal methods would ideally have their calls be assigned to a variable.
+    public Card dealTop(){
+        Card output;
+        output = inventory.get(inventory.size() - 1);
+        inventory.remove(inventory.size() - 1);
+        return output;
+    }
+
+    public Card dealIndex(int index){
+        Card output;
+        output = inventory.get(index);
+        inventory.remove(index);
+        return output;
+    }
+
+
+    public String toString(){
+        String output = "";
+        for (int i = 1; i < inventory.size(); i++) {
+            output += inventory.get(i) + "\n";
+        }
+        return output;
+    }
 }

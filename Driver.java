b43b0fa7826596b0210fec368526c31a5dbecef6
dylanmas <dylanmas687@gmail.com>
2021-03-13 20:16:37 -0600

    Added War.java

--- Driver.java
+++ Driver.java
@@ -1,24 +1,11 @@
+import java.util.*;
+
 public class Driver {
     public static void main(String[] args) {
-        Card test = new Card(13, 2);
-        Card foo = new Card(1, 2);
-        System.out.println(test.getSuitName() + "jfsdjlkfsdjklfdsjkldsfakjl");
-        System.out.println(test.compareTo(foo));
+        Scanner input = new Scanner(System.in);
         
-        Deck lol = new Deck(1);
-        System.out.println(lol);
-
-        Player pl1 = new Player("Johannesburg");
-        Player pl2 = new Player("CapeTown");
-
-        lol.shuffleCards();
-        lol.deal(pl1, pl2);
-
-        System.out.println(pl1);
-        System.out.println(pl2 + "DIVIDER");
-
-        Card fff = pl2.dealTop();
-
-        System.out.println("\n" + fff.toString() + "\n\t\t\tHere's the new cards" + pl2);
+        System.out.print("Welcome to War!\nTo begin playing, type in Player 1's name:");    
+        String p1name = input.nextLine();
+        System.out.print("Now, type in Player 2's name: ")
     }
 }
---
+++ War.java
@@ -0,0 +1,19 @@
+/*
+Date:
+    3/1/2021
+Coder name:
+    Dylan Masulis
+Project name:
+    Card Game
+*/
+public class War {
+    Player p1;
+    Player p2;
+
+    public War (String p1name, String p2name){
+        p1 = new Player(p1name);
+        p2 = new Player(p2name);
+
+        
+    }
+}
